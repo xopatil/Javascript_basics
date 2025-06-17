898fe0002c8172c801f51073b1564a88c8fb40b4
xopatil <sohampatil1510@gmail.com>
2025-06-17 19:57:02 +0530

    new quesiton 7 change

--- Homework 2/q7.js
+++ Homework 2/q7.js
@@ -1,16 +1,32 @@
-function addtoCart(itemStock) {
-    if (itemStock == 0){
-      throw new Error("item is out of stock, please try again later ")
-    }
-}
-function meth1() {
-    addtoCart(0);
-}
-function meth2() {
-    try {
-        meth1()
-    } catch (error) {
+// function addtoCart(itemStock) {
+//     if (itemStock == 0){
+//       throw new Error("item is out of stock, please try again later ")
+//     }
+// }
+// function meth1() {
+//     addtoCart(0);
+// }
+// function meth2() {
+//     try {
+//         meth1()
+//     } catch (error) {
+//         console.log(error.name+" : "+error.message);
+//     }
+// }
+// meth2()
+
+function addtoCart(){
+    try{
+        checkStock(2);
+        console.log("item added to cart");  
+    }catch(error){
         console.log(error.name+" : "+error.message);
     }
+
 }
-meth2()
+function checkStock(stock){
+    if (stock <3){
+        throw new Error("produc count is less than 3, please restock");      
+    }
+}
+addtoCart()
